295b7b91e3d14dd5a2699fdcd2abe98ffaab3160
sam <swannsee20@gmail.com>
2018-01-06 08:58:58 -0800

    widget update

---
+++ app/src/main/java/com/example/sam/mebake/widget/NewAppRemoteViewsService.java
@@ -0,0 +1,8 @@
+package com.example.sam.mebake.widget;
+
+/**
+ * Created by sam on 1/5/18.
+ */
+
+public class NewAppRemoteViewsService {
+}
--- app/src/main/java/com/example/sam/mebake/widget/NewAppWidget.java
+++ app/src/main/java/com/example/sam/mebake/widget/NewAppWidget.java
@@ -1,14 +1,19 @@
-package com.example.sam.mebake;
+package com.example.sam.mebake.widget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.widget.RemoteViews;
+
+import com.example.sam.mebake.R;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class NewAppWidget extends AppWidgetProvider {
+
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
@@ -25,9 +30,16 @@ public class NewAppWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        for (int i = 0; i < appWidgetIds.length; i ++ ){
+            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+            Intent intent = new Intent(context, NewAppRemoteViewsService.class);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+            rv.setRemoteAdapter(R.id.widget_listview, intent);
+            appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
         }
+
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
     @Override
