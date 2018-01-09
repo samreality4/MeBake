769a389118741a731c25b21f927cd4a32b41237f
sam <swannsee20@gmail.com>
2018-01-08 19:34:51 -0800

    widget updates from mac

--- app/src/test/java/com/example/sam/mebake/TestActivity.java
+++ app/src/test/java/com/example/sam/mebake/TestActivity.java
@@ -1,14 +1,42 @@
 package com.example.sam.mebake;
 
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+import java.sql.Statement;
+
+import static android.support.test.espresso.Espresso.onView;
+
 
 /**
  * Created by sam on 1/8/18.
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(JUnit4.class)
+@LargeTest
 public class TestActivity {
 
     @Rule
-    public ActivityTestRule<RecipeDetail> m
+    public TestRule mTestRule = new TestRule() {
+        @Override
+        public Statement apply(Statement base, Description description) {
+            return null;
+        }
+
+    }
+
+        @Test
+        public void RecipeName_Check()[]{
+                onView()
+        }
+
+    }
+
 }
